9d89bc7db8196eb9f1417ed71671182479b058ee
moaj257 <moaj257@gmail.com>
2023-05-24 12:46:14 +0530

    Workflow Changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     modules: ['@nuxtjs/tailwindcss'],
-    runtimeConfig: {
-        "app": {
-          "baseURL": "/betatube",
-          "buildAssetsDir": "/betatube/_nuxt/",
-        }
-      }
 })
